27cee607a918d49f2b8adc8cf90cfd142182549a
megaarman <>
2018-05-14 15:41:42 -0400

    facts file now includes scorename key

--- backend_engine/writeFacts.js
+++ backend_engine/writeFacts.js
@@ -2,6 +2,7 @@ console.time("Took");
 const fs = require("fs");
 const computeFacts = require("./computeFacts.js");
 
+//invoke with "node writeFacts.js ..path to scoredir.. facts.json"
 const scoreDir = process.argv[2];
 const factsFile = process.argv[3];
 const scoreNames = fs.readdirSync(scoreDir);
@@ -11,8 +12,8 @@ scoreNames.forEach((scoreName) =>
 {
   console.log(scoreDir + scoreName + "...");
   const musicxml = fs.readFileSync(scoreDir + scoreName);
-  const scoreFacts = {};
-  scoreFacts[scoreName] = computeFacts(musicxml);
+  const scoreFacts = computeFacts(musicxml);
+  scoreFacts["scoreName"] = scoreName;
   factsDB.push(scoreFacts);
 });
 
