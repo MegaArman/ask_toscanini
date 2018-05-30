eda8baa92ddfc58ca2e27753b2b9585a5915430f
megaarman <>
2018-05-30 16:49:12 -0400

    can search parsed string, need make parser fault tolerant?

--- backend_engine/searchDB.js
+++ backend_engine/searchDB.js
@@ -1,48 +1,50 @@
 const MongoClient = require("mongodb").MongoClient;
 const assert = require("assert");
-//const musicql = require("./musicql.js");
+const MQL = require("./musicql.js");
 
 const url = "mongodb://localhost:27017";
 const dbName = "askToscanini";
 const collectionName = "scoreFacts";
 
-const findDocuments = function(db, callback) 
-{
-  // Get the documents collection
-  const collection = db.collection(collectionName);
-
-  collection.find(
-    {"$and":
-    [{"_id":{"$regex":"beethoven"}}, 
-    {"minTempo":{"$gte":70}},{"maxTempo":{"$lte":170}},
-    {"instrumentRanges.instrumentName":{"$regex":"flute"},
-     "instrumentRanges.minPitch":{"$gte":50},
-     "instrumentRanges.maxPitch":{"$lte":80}},
-    {"instrumentRanges.instrumentName":{"$regex":"trumpet"},
-    "instrumentRanges.minPitch":{"$gte":42},
-    "instrumentRanges.maxPitch":{"$lte":69}},
-    {"keySignatures":"Bb"}, 
-    {"timeSignatures":{"beats":2,"beatType":4}}
-    ]}) 
-    .project({_id: 1})
-    .toArray(function(err, docs) 
-    {
-      assert.equal(err, null);
-      console.log("Found the following scores");
-      console.log(docs);
-      callback(docs);
-  });
-};
 
 // Use connect method to connect to the server
-MongoClient.connect(url, function(err, client) 
-{
-  assert.equal(null, err);
-  console.log("Connected successfully to server");
- 
-  const db = client.db(dbName);
-  findDocuments(db, function()
+module.exports = (queryString) =>
   {
-    client.close();
+  MongoClient.connect(url, function(err, client) 
+  {
+    const findDocuments = function(db, callback) 
+    {
+      // Get the documents collection
+      const collection = db.collection(collectionName);
+      let mongoQueryObj; 
+      try 
+      {
+        mongoQueryObj = MQL.parse(queryString)[1];
+      }
+      catch (e)
+      {
+        console.log(e);
+        return "cannot search on this string";
+      }
+
+      collection.find(mongoQueryObj) 
+        .project({_id: 1})
+        .toArray(function(err, docs) 
+        {
+          assert.equal(err, null);
+          console.log("Found the following scores");
+          console.log(docs);
+          callback(docs);
+      });
+    };
+
+    assert.equal(null, err);
+    console.log("Connected successfully to server");
+
+    const db = client.db(dbName);
+    findDocuments(db, function()
+    {
+      client.close();
+    });
   });
-});
+};
---
+++ backend_engine/useSearchDB.js
@@ -0,0 +1,3 @@
+const searchDB = require("./searchDB.js");
+
+searchDB("Beethoven and tempo 70 170 and flute D4 G#6 and trumpet F#3 A5 and key Bb and ts 2 4");
