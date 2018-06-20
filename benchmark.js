e0a22bd69fa686cf4273ac649e58bbc10b7a47c6
megaarman <>
2018-06-20 18:49:03 -0400

    benchmark 1 measures time correctly

--- benchmark.js
+++ benchmark.js
@@ -6,25 +6,27 @@ const queryString = "tempo 100 150";
 let db;
 
 MongoClient.connect("mongodb://localhost:27017", {useNewUrlParser: true},
-async function(err, client) 
+function(err, client) 
 {
   if(err) throw err;
   db = client.db("askToscanini");
 
+  console.time("took");
   for (let i = 0; i < 100; i++)
   {
-    console.time("took");
     const mongoQueryObj = MQL.parse(queryString)[1];
 
     // console.log("mongoQueryObj", JSON.stringify(mongoQueryObj));
-    await db.collection("scoreFacts").distinct("_id", mongoQueryObj,
+    db.collection("scoreFacts").distinct("_id", mongoQueryObj,
     (err, scoreNames) =>
     { 
       if (err) console.log(err);
       let result;
       result = JSON.stringify(scoreNames);
+      //console.log("done " + i); //proof time is being measured correct
+      console.timeEnd("took");
+      console.time("took");
     });
-  console.timeEnd("took");
   }//end loop
   client.close();
 });
